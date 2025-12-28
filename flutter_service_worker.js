29d9138a9af345f0d7c4ee5eb900ed5bb51c8b09
Bert Gollnick <info@gollnickdata.de>
2025-12-28 11:45:47 +0100

    imprint and cookie policy added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -300,14 +300,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a183cc5a57d85221a0e7674c0d239f90
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "d510807ec8b51ba81cb24669f6b666b9",
+"flutter_bootstrap.js": "f79c7b7fbdc15c9a5f90a1f69ae98aaa",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7022e12410be3dfa45400322ac92056a",
 "/": "7022e12410be3dfa45400322ac92056a",
-"main.dart.js": "8eae4b29a6a0d222f3edb9f4a6889943",
+"main.dart.js": "be15a6836f532afe910f0680ebba43cc",
 "manifest.json": "2b4ee48df98b4a78a958ff6680f5e34e",
 "version.json": "e5032fe7579341bea05060605c36227b"};
 // The application shell files that are downloaded before a service worker can
